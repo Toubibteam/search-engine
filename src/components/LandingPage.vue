ab1edd66e30ddc1a57aa834e9522a19b069eb059
Baboo7 <baptiste.studer@laposte.net>
2018-01-25 12:20:34 +0100

    feature/add backgound picture and and link to search engine on landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,39 +1,49 @@
 <template>
   <div class="container-fluid p-0">
     <div id="billboard" class="m-0 p-0">
+      <div class="background"> </div>
+      <div class="filter"> </div>
       <div id="logo" class="align-middle">
-        <img src="../assets/logo-blue.png" class="img-fluid">
+        <img src="../assets/logo-txt-white.png" class="img-fluid">
       </div>
     </div>
 
     <!-- <router-link to="codage">Moteur de recherche</router-link> -->
 
     <div id="content" class="my-5 px-2">
-      <h1 class="my-4">Pourquoi utiliser Toubib ?</h1>
+      <section>
+        <h3 class="my-4">Essayez notre <span class="font-bold">moteur de recherche intelligent</span> !</h3>
 
-      <div class="row">
-        <div class="col-md-4 py-3">
-          <div class="icon">
-            <div class="square rounded-circle"> </div>
-            <img src="../assets/computer.png" class="img-fluid">
+        <p>Accéder au <router-link to="codage">moteur de recherche intelligent</router-link></p>
+      </section>
+
+      <section>
+        <h3 class="my-4">Pourquoi utiliser <span class="font-bold">Toubib</span> ?</h3>
+
+        <div class="row">
+          <div class="col-md-4 py-3">
+            <div class="icon">
+              <div class="square rounded-circle"> </div>
+              <img src="../assets/computer.png" class="img-fluid">
+            </div>
+            <p>Une navigation intuitive et fluide, conçue par et pour les codeurs médicaux</p>
           </div>
-          <h4>Une navigation intuitive et fluide, conçue par et pour les codeurs médicaux</h4>
-        </div>
-        <div class="col-md-4 py-3">
-          <div class="icon">
-            <div class="square rounded-circle"> </div>
-            <img src="../assets/books.png" class="img-fluid">
+          <div class="col-md-4 py-3">
+            <div class="icon">
+              <div class="square rounded-circle"> </div>
+              <img src="../assets/books.png" class="img-fluid">
+            </div>
+            <p>Un contenu régulièrement mis à jour et enrichi par les contributions de nos utilisateurs</p>
           </div>
-          <h4>Un contenu régulièrement mis à jour et enrichi par les contributions de nos utilisateurs</h4>
-        </div>
-        <div class="col-md-4 py-3">
-          <div class="icon">
-            <div class="square rounded-circle"> </div>
-            <img src="../assets/magnifier.png" class="img-fluid">
+          <div class="col-md-4 py-3">
+            <div class="icon">
+              <div class="square rounded-circle"> </div>
+              <img src="../assets/magnifier.png" class="img-fluid">
+            </div>
+            <p>La garantie de trouver le bon code en un clic grâce à notre moteur de recherche intelligent</p>
           </div>
-          <h4>La garantie de trouver le bon code en un clic grâce à notre moteur de recherche intelligent</h4>
         </div>
-      </div>
+      </section>
     </div>
   </div>
 </template>
@@ -49,10 +59,33 @@ export default {
 
 #billboard {
   display: table;
-  background-color: #2d2281;
+  position: relative;
   width: 100%;
   min-height: 50vh;
   height: 50vh;
+}
+
+#billboard > .background {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-image: url('../assets/microscope.jpg');
+  background-position: center;
+  background-size: cover;
+  z-index: -20;
+}
+
+#billboard > .filter {
+  background-color: #2d2281;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: -10;
+  opacity: 0.5;
 }
 
 #logo {
@@ -62,6 +95,10 @@ export default {
 #content {
   max-width: 1000px;
   margin: auto;
+}
+
+.font-bold {
+  font-weight: bold;
 }
 
 .icon {
