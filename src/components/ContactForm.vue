5bc58975113bece463741f542116cffe459febae
Baboo7 <baptiste.studer@laposte.net>
2018-02-09 10:08:20 +0100

    feature/create and integrate contact form component

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <form class="container-fluid m-0 p-0 w-100">
+    <div class="form-group">
+      <label for="fullnameInput">Prénom & Nom</label>
+      <input type="text" class="form-control" id="fullnameInput" placeholder="Greg House">
+    </div>
+    <div class="form-group">
+      <label for="establishmentInput">Établissement</label>
+      <input type="text" class="form-control" id="establishmentInput" placeholder="Princeton Plainsboro Teaching Hospital">
+    </div>
+    <div class="form-group">
+      <label for="phoneInput">Tél.</label>
+      <input type="text" class="form-control" id="phoneInput" placeholder="+33x xx xx xx xx">
+    </div>
+    <div class="form-group">
+      <label for="emailInput">Email</label>
+      <input type="text" class="form-control" id="emailInput" placeholder="ghouse@ppth.com">
+    </div>
+    <button type="submit" class="btn btn-primary"> <i class="fas fa-paper-plane"></i> Envoyer </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ContactForm'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../styles/colors.scss";
+
+* {
+  text-align: left;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import VueResource from 'vue-resource'
 
 import App from './App'
 import CodeItem from './components/CodeItem'
+import ContactForm from './components/ContactForm'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import SearchEngine from './components/SearchEngine'
@@ -17,6 +18,7 @@ Vue.use(VueResource)
 Vue.http.options.emulateJSON = true
 
 Vue.component('code-item', CodeItem)
+Vue.component('app-contact-form', ContactForm)
 Vue.component('app-footer', Footer)
 Vue.component('app-header', Header)
 Vue.component('app-search-engine', SearchEngine)
